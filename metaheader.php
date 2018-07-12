fcab99a354a42ac2941bdcd6cd1bbafcd8f913e8
YENG QUISPE <yengquispe@gmail.com>
2018-07-12 03:25:33 -0400

    Update metaheader.php

--- metaheader.php
+++ metaheader.php
@@ -6,13 +6,13 @@
 ?>
 <title>Agroinsuca C.A</title>
 <meta charset="utf-8"/>
-<link rel="stylesheet" href="./bootstrap/dist/css/bootstrap.min.css"/>
-<link rel="stylesheet" href="./bootstrap/dist/css/bootstrap-theme.min.css"/>
-<link rel="stylesheet" href="./slideryq.css"/>
-<script src="./jquery.js"></script>
-<script src="./slideryq.js"></script>
-<script src="./bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="./bootstrap/dist/js/npm.js"></script>
+<link rel="stylesheet" href="https://github.com/moro155/agroinsuca/bootstrap/dist/css/bootstrap.min.css"/>
+<link rel="stylesheet" href="https://github.com/moro155/agroinsuca/bootstrap/dist/css/bootstrap-theme.min.css"/>
+<link rel="stylesheet" href="https://github.com/moro155/agroinsuca/slideryq.css"/>
+<script src="https://github.com/moro155/agroinsuca/jquery.js"></script>
+<script src="https://github.com/moro155/agroinsuca/slideryq.js"></script>
+<script src="https://github.com/moro155/agroinsuca/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="https://github.com/moro155/agroinsuca/bootstrap/dist/js/npm.js"></script>
 <script>
 
 </script>
